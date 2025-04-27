6effc0bb42752ae6e38ec22f1b1ac78855f1c4d7
Aloh Michael <mykelaloh@gmail.com>
2025-04-27 16:29:00 +0100

    update settings.py to allo railway host

--- aitranscribe/settings.py
+++ aitranscribe/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7n%%2nw!2822!@h-m*-r_u80)@ani(e@ak!9&b@ql9fn(qkz+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aitranscribe-production.up.railway.app']
+
 
 
 # Application definition
